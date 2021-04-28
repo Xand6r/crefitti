4cea22a2d8b9f51b9371af1f68ff179314fb6117
xander <shuaibualexander@gmail.com>
2021-04-28 13:30:05 +0100

    make little updates

--- styles/animate_identity.js
+++ styles/animate_identity.js
@@ -2,27 +2,27 @@ const elements={
   1:{
     upperText:"we aren't normal.",
     lowerText:"creativity is the closest thing to normality for us, it's the only way we can bear this world. it's our seal.",
-    image:"./images/assets/ccrefitti.svg"
+    image:"./images/assets/ccrefitti.png"
   },
   2:{
     upperText:"we don’t grope in the dark. and yes, our ",
     lowerText:"creativity is not from the dark arts. we love to explore and find out why, and our favorite part, how we can create with the discoveries and insights.",
-    image:"./images/assets/rcrefitti.svg"
+    image:"./images/assets/rcrefitti.png"
   },  
   3:{
     upperText:"like a powerhouse, each native of the clan",
     lowerText:" contributes their positive energy cells to everything we do, whether we are at work or having a good time with friends.",
-    image:"./images/assets/ecrefitti.svg"
+    image:"./images/assets/ecrefitti.png"
   },
   4:{
     upperText:"we dare to dream of the future, and",
     lowerText:"we dare to create it too. we know no more fun way to live life each day.",
-    image:"./images/assets/f1crefitti.svg"
+    image:"./images/assets/f1crefitti.png"
   },
   5:{
     upperText:"the freedom to explore, fail and repeat",
     lowerText:"is a huge part of who we are.",
-    image:"./images/assets/f2crefitti.svg"
+    image:"./images/assets/f2crefitti.png"
   }
 }
 
